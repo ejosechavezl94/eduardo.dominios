86ebf6c992a764ccf0fc8bcf02bf2b5c98f2827e
Eduardo Chavez <ejosechavezl18@gmail.com>
2025-02-22 16:57:46 +0000

    Dominio Entrega

---
+++ app.js
@@ -0,0 +1,12 @@
+const generadorDominio = () => {
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    for (let p of pronoun)
+        for (let a of adj)
+            for (let n of noun)
+
+                console.log(p + a + n + `.com`);
+}
+
+generadorDominio()
